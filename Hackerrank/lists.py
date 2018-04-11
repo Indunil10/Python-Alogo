a0111a2cbcf133d2821affeb7538d9394443d492
indunil maduranga <indunilm76@gmail.com>
2018-04-11 10:37:28 +0530

    Python lists

---
+++ Hackerrank/lists.py
@@ -0,0 +1,18 @@
+list1 = [3,6,9,12,15]
+list2 = [2,4,6,8,10]
+print(list1)
+print(list1 + list2)
+
+list3 =["Indunil",10,"Methmi"]
+
+print(list3)
+list3.append("Gems in srilanka")
+
+print(list3)
+del list3[3]
+print(list3)
+
+list4 =[5,6,1,42,20,10]
+
+list4.sort()
+print(list4)
