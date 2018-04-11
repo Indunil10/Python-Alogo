7062442ecbe7e4195831ac82a34ac48046e48587
indunil maduranga <indunilm76@gmail.com>
2018-04-11 09:52:34 +0530

    Strings in python

---
+++ Hackerrank/Strings.py
@@ -0,0 +1,42 @@
+'''name = "Indunil"
+
+length = len(name);
+
+i = 0
+
+for n in range(-1,(-length -1),-1):
+    print (name[n],"\t",name[i])
+    i+= 1
+    
+# Concatenation 
+
+print("Indunuil" + "Maduranga")
+
+#String Replication
+
+print(5*"You")
+
+#String Membership
+
+str1 = "Sri lanka is my country USA is my favorite country"
+str2 ="Sri lanka"
+str3 ="England"
+
+print(str2 in str1)
+print(str3 in str1)
+'''
+# Relational Operators
+
+print("A" < "a")
+print("ABC" =="ABC")
+print("B" == "C")
+
+#String Slice
+
+str4 = "javatpoint"
+
+print(len(str4))
+print(str4[2:])
+print(str4[2:10])
+
+
